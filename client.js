fa8fe6dadd87c997e22c6234a258c6f02aa91467
Chris Tate <chris_tate_94@hotmail.com>
2022-01-19 21:13:37 +0000

    added a name for the snake

--- client.js
+++ client.js
@@ -8,11 +8,24 @@ const connect = function () {
     port: 50541,
   });
 
-  conn.setEncoding("utf8");
   conn.on("data", (data) => {
     console.log(`Server data: ${data}`);
   });
 
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: CTF");
+
+    // setInterval(() => {
+    //   conn.write("Move: left");
+    // }, 100);
+  });
+
+  conn.on("end", () => {
+    console.log("Disconnected");
+  });
+
+  conn.setEncoding("utf8");
   return conn;
 };
 module.exports = { connect };
